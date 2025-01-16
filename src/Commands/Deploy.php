e4b9934b3e9f105293df63379c09a2c48c5045d8
Matt Lantz <mattlantz@gmail.com>
2025-01-15 23:31:38 -0500

    because of deployment help

---
+++ src/Commands/Deploy.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Grafite\Blacksmith\Commands;
+
+use Laravel\Forge\Forge;
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+
+class Deploy extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'blacksmith:deploy {--server=} {--site=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deploy a Forge site.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $forge = new Forge(config('blacksmith.forge_token'));
+        $serverIds = [$this->option('server')];
+
+        if (! $this->option('server')) {
+            $serverIds = collect(glob(base_path('.blacksmith') . '/*' , GLOB_ONLYDIR))->map(function ($server) {
+                return Str::of($server)->replace(base_path('.blacksmith/'), '')->toString();
+            })->toArray();
+        }
+
+        foreach ($serverIds as $serverId) {
+            $basePath = base_path('.blacksmith/'.$serverId.'/');
+            $config = json_decode(file_get_contents($basePath.'config.json'), true);
+            $siteConfigs = $config['sites'];
+
+            if ($this->option('site')) {
+                $config = collect($config['sites'])->where('id', $this->option('site'))->first();
+                $siteConfigs = ['sites' => $config];
+            }
+
+            foreach ($siteConfigs as $config) {
+                $siteId = $config['id'];
+
+                $forge->post("servers/$serverId/sites/$siteId/deployment/deploy");
+
+                $this->info('Site deployment triggered.');
+            }
+        }
+
+        return 0;
+    }
+}
